fe1086183d3bdd6b4247741015a6e62f0d48d79f
derrickweru <derrickweru73@gmail.com>
2026-07-14 21:14:08 +0300

    feat: edit the components spelling errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import StudentDashboard from "./pages/StudentDashboard";
+import Home from "./pages/Home";
  
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
         <Route path="/student-dashboard" element={
           <ProtectedRoute>
              < StudentDashboard/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Hero from "../components/home/Hero";
-import Stats from "../components/home/Stats";
-import CoursesPreview from "../components/home/CoursesPreview";
-import WhyChooseUs from "../components/home/WhyChooseUs";
-import Reviews from "../components/home/Reviews";
-import FAQ from "../components/home/FAQ";
-import CTA from "../components/home/CTA";
-import Footer from "../components/home/Footer";
+import Hero from "@/components/Hero";
+import Stats from "@/components/Stats";
+import CoursesPreview from "@/components/CoursesPreview";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Reviews from "@/components/Reviews";
+import FAQ from "@/components/FAQ";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
