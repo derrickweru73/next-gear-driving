834950e94e22a4cba5261fe1f748bde80ef3d6e6
derrickweru <derrickweru73@gmail.com>
2026-07-14 21:48:28 +0300

    feat: removing  the navbar from the ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import React from "react";
-import Navbar from "./components/ui/Navbar";
+import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoute from "./components/ProtectedRoute";
 import StudentDashboard from "./pages/StudentDashboard";
 import Home from "./pages/Home";
- 
+import Reviews from "./components/Reviews";
+import WhyChooseUs from "./components/WhyChooseUs";
+import FAQ from "./components/FAQ";
 function App() {
   return (
     <>
        <Navbar />
       <Routes>
+
+         
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/whyChooseus" element={<WhyChooseUs />} />
+        <Route path="/faq" element={<FAQ />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/" element={<Home />} />
