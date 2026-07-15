8c7989e81c843b3134504707e4f35ec5281d7015
derrickweru <derrickweru73@gmail.com>
2026-07-15 15:22:19 +0300

    feat: add multi-role users api for student, instructor, and admin

--- src/services/AuthApi.js
+++ src/services/AuthApi.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 
 const API_URL =
-  "https://6a5608ffb17de7bebbddbc73.mockapi.io/api/students";
+  "https://6a5608ffb17de7bebbddbc73.mockapi.io/api/users";
 
-// Register Student
-export const registerStudent = async (studentData) => {
-  const response = await axios.post(API_URL, studentData);
+// Register User
+export const registerUser = async (userData) => {
+  const response = await axios.post(API_URL, userData);
   return response.data;
 };
 
-// Get All Students
-export const getStudents = async () => {
+// Get All Users
+export const getUsers = async () => {
   const response = await axios.get(API_URL);
   return response.data;
 };
 
-// Login Student
-export const loginStudent = async (email, password) => {
+// Login User
+export const loginUser = async (email, password) => {
   const response = await axios.get(API_URL);
 
-  const student = response.data.find(
+  return response.data.find(
     (user) =>
       user.email === email &&
       user.password === password
   );
-
-  return student;
 };
