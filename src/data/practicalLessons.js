68daee9916d039ff9421eb1a131cf1e6591f757f
derrickweru <derrickweru73@gmail.com>
2026-07-16 10:52:16 +0300

    create: practical lesson js

---
+++ src/data/practicalLessons.js
@@ -0,0 +1,28 @@
+const practicalLessons = [
+  {
+    id: 1,
+    title: "Vehicle Controls",
+    duration: "1 Hour",
+    location: "Training Ground",
+  },
+  {
+    id: 2,
+    title: "Moving Off & Stopping",
+    duration: "1 Hour",
+    location: "Training Ground",
+  },
+  {
+    id: 3,
+    title: "Gear Changing",
+    duration: "1 Hour",
+    location: "City Roads",
+  },
+  {
+    id: 4,
+    title: "Parking",
+    duration: "1 Hour",
+    location: "City Roads",
+  },
+];
+
+export default practicalLessons;
